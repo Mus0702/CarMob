43a571ca7abf8a83f6d68efeb97f70bd5bc64dce
Mustafa <azoud@hotmail.com>
2023-07-09 19:51:48 +0200

    initial commit

---
+++ src/main/java/com/azoudmustafa/CarMobApplication.java
@@ -0,0 +1,13 @@
+package com.azoudmustafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMobApplication.class, args);
+	}
+
+}
