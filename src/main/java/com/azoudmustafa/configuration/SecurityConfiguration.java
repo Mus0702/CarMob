adec6014700adbba728b0f96cf50c4d6ba911dcf
Mustafa Azoud <Mustafa.azoud@inetum-realdolmen.world>
2023-07-24 11:42:43 +0200

    354 feat: add configuations files

---
+++ src/main/java/com/azoudmustafa/configuration/SecurityConfiguration.java
@@ -0,0 +1,70 @@
+package com.azoudmustafa.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    private final CustomAuthEntryPoint customAuthEntryPoint;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors();
+        http
+                .csrf();
+
+//                .csrf()
+//                .disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/register/**", "/login/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(customAuthEntryPoint);
+
+        return http.build();
+
+//        http
+//                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+//                .and()
+//                .csrf().disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/register/**", "/login/**", "/airport/**", "/flight/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .exceptionHandling()
+//                .authenticationEntryPoint(customAuthEntryPoint)
+//                .accessDeniedHandler(new AccessDeniedHandlerImpl())
+//                .and()
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+    }
+
+}
