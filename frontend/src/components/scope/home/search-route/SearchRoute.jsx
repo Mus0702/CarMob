14e74772465b3d341bd7e13e09e018199036eee9
Mustafa Azoud <Mustafa.azoud@inetum-realdolmen.world>
2023-08-21 10:21:48 +0200

    280 feat: add address autocompletion using google map api

--- frontend/src/components/scope/home/search-route/SearchRoute.jsx
+++ frontend/src/components/scope/home/search-route/SearchRoute.jsx
@@ -1,37 +1,29 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
+import { useLoadScript, StandaloneSearchBox } from "@react-google-maps/api";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./SearchRoute.css";
 import { useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+
+const libraries = ["places"];
 
 const SearchRoute = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_REACT_APP_GOOGLE_API_KEY,
+    libraries,
+  });
+
   const [selectedDepartureAddress, setSelectedDepartureAddress] = useState("");
   const [selectedArrivalAddress, setSelectedArrivalAddress] = useState("");
   const [selectedDepartureDate, setSelectedDepartureDate] = useState(null);
   const [selectedNumberSeats, setSelectedNumberSeats] = useState("");
   const navigate = useNavigate();
 
-  function debounce(fn, delay) {
-    let timeoutID;
-    return function (...args) {
-      if (timeoutID) {
-        clearTimeout(timeoutID);
-      }
-      timeoutID = setTimeout(() => {
-        fn(...args);
-      }, delay);
-    };
-  }
-
-  // Fonctions debounce pour la mise à jour des adresses
-  const debouncedSetSelectedDepartureAddress = debounce((value) => {
-    setSelectedDepartureAddress(value);
-  }, 300);
-
-  const debouncedSetSelectedArrivalAddress = debounce((value) => {
-    setSelectedArrivalAddress(value);
-  }, 300);
+  const departureSearchBoxRef = useRef(null);
+  const arrivalSearchBoxRef = useRef(null);
 
   const handleSearchClick = () => {
     const queryParams = new URLSearchParams();
@@ -42,42 +34,63 @@ const SearchRoute = () => {
       dayjs(selectedDepartureDate).format("YYYY-MM-DD"),
     );
     queryParams.append("numberOfSeats", selectedNumberSeats);
-
     navigate(`/route-results?${queryParams.toString()}`);
   };
 
+  const onDeparturePlacesChanged = () => {
+    const places = departureSearchBoxRef.current.getPlaces();
+    if (places && places.length === 1) {
+      setSelectedDepartureAddress(places[0].formatted_address);
+    }
+  };
+
+  const onArrivalPlacesChanged = () => {
+    const places = arrivalSearchBoxRef.current.getPlaces();
+    if (places && places.length === 1) {
+      setSelectedArrivalAddress(places[0].formatted_address);
+    }
+  };
+
+  if (!isLoaded) return "Loading...";
+
   return (
     <div className="container h-100">
-      <div className=" d-flex justify-content-center align-items-center h-100 mt-3">
-        <input
-          type="text"
-          className="form-control w-50"
-          value={selectedDepartureAddress}
-          onChange={(event) => setSelectedDepartureAddress(event.target.value)}
-          placeholder="Departure address"
-        />
-        <input
-          type="text"
-          className="form-control w-50"
-          value={selectedArrivalAddress}
-          onChange={(event) => setSelectedArrivalAddress(event.target.value)}
-          placeholder="Arrival address"
-        />
+      <div className=" d-flex  mt-3">
+        <StandaloneSearchBox
+          onLoad={(ref) => (departureSearchBoxRef.current = ref)}
+          onPlacesChanged={onDeparturePlacesChanged}
+        >
+          <input
+            type="text"
+            className="form-control w-100"
+            placeholder="Departure address"
+          />
+        </StandaloneSearchBox>
+        <FontAwesomeIcon icon={faLocationDot} bounce size="xl" />
+
+        <StandaloneSearchBox
+          onLoad={(ref) => (arrivalSearchBoxRef.current = ref)}
+          onPlacesChanged={onArrivalPlacesChanged}
+        >
+          <input
+            type="text"
+            className="form-control w-100"
+            placeholder="Arrival address"
+          />
+        </StandaloneSearchBox>
+        <FontAwesomeIcon icon={faLocationDot} bounce size="xl" />
 
         <DatePicker
-          showPopperArrow
-          calendarClassName="react-datepicker__calendar-icon"
+          selected={selectedDepartureDate}
+          className="form-control w-100"
+          onChange={(date) => setSelectedDepartureDate(date)}
           dateFormat="dd/MM/yyyy"
           placeholderText="DD/MM/YYYY"
-          selected={selectedDepartureDate}
-          onChange={(date) => {
-            setSelectedDepartureDate(date);
-            console.log({ selectedDepartureDate });
-          }}
         />
+
         <input
           type="number"
-          className="form-control w-25"
+          className="form-control w-auto"
           value={selectedNumberSeats}
           onChange={(event) => setSelectedNumberSeats(event.target.value)}
           placeholder="number of passengers"
@@ -89,61 +102,6 @@ const SearchRoute = () => {
       </div>
     </div>
   );
-
-  //   <div>
-  //     <div className="d-flex flex-row align-items-center mb-4">
-  //       <GooglePlacesAutocomplete
-  //         onSelect={({ description }) =>
-  //           debouncedSetSelectedDepartureAddress(description)
-  //         }
-  //         placeholder="Departure Address"
-  //       />
-  //     </div>
-  //
-  //     <div className="d-flex flex-row align-items-center mb-4">
-  //       <GooglePlacesAutocomplete
-  //         onSelect={({ description }) =>
-  //           debouncedSetSelectedArrivalAddress(description)
-  //         }
-  //         placeholder="Arrival Address"
-  //       />
-  //     </div>
-  //
-  //     <div className="d-flex flex-row align-items-center mb-4">
-  //       <div className="form-outline flex-fill mb-0">
-  //         <label className="form-label" htmlFor="birthdate"></label>
-  //         <DatePicker
-  //           id="selectedDepartureDate"
-  //           showIcon
-  //           className="form-control"
-  //           calendarClassName="custom-calendar"
-  //           clearIcon={null}
-  //           format="dd/MM/yyyy"
-  //           minDate={new Date()}
-  //           placeholder="DD/MM/YYYY"
-  //           value={selectedDepartureDate}
-  //           onChange={(date) => setSelectedDepartureDate(date)}
-  //         />
-  //       </div>
-  //     </div>
-  //
-  //     <div className="d-flex flex-row align-items-center mb-4">
-  //       <div className="form-outline flex-fill mb-0">
-  //         <label className="form-label" htmlFor="form3Example3c"></label>
-  //         <input
-  //           type="number"
-  //           id="numberSeat"
-  //           className={`form-control`}
-  //           placeholder="Number of passenger"
-  //           value={selectedNumberSeats}
-  //           onChange={(event) => setSelectedNumberSeats(event.target.value)}
-  //         />
-  //       </div>
-  //     </div>
-  //
-  //     <button>Rechercher</button>
-  //   </div>
-  // );
 };
 
 export default SearchRoute;
