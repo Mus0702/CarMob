d62651ab6d0ef07030367023a9a226f9b75fb44d
Mustafa Azoud <Mustafa.azoud@inetum-realdolmen.world>
2023-10-09 12:02:48 +0200

    change time of sending a mail

--- backend/src/main/java/com/azoudmustafa/service/notification/EmailNotificationService.java
+++ backend/src/main/java/com/azoudmustafa/service/notification/EmailNotificationService.java
@@ -19,7 +19,7 @@ public class EmailNotificationService {
     @Autowired
     private EmailService emailService;
 
-  @Scheduled(cron = "0 5 15 * * ?")
+  @Scheduled(cron = "0 30 11 * * ?")
     public void sendRatingEmailsForPreviousDayTrips() {
        LocalDate currentDate = LocalDate.now();
         List<Booking> bookings = bookingRepository.findBookingsToNotify(currentDate);
